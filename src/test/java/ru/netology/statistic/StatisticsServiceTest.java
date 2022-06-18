5d3c0962ea9c0e06181c42a9f2d56a06ac1cbe42
Girych <dmitriev.ilya@mail.ru>
2022-06-18 13:39:36 +0300

    commit2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -21,8 +21,8 @@ public class StatisticsServiceTest {
         void findMaxInTheMiddle() {
             StatisticsService service = new StatisticsService();
 
-            long[] incomesInBillions = {12, 5, 8, 4, 60, 3, 8, 6, 11, 11, 12};
-            long expected = 60;
+            long[] incomesInBillions = {12, 5, 8, 4, 60, 3, 8, 6, 11, 11, 70};
+            long expected = 70;
 
             long actual = service.findMax(incomesInBillions);
 
